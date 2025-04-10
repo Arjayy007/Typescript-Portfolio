5a35ec0330c7857f6293a5ba66c2ae7322f9e599
Arjayy007 <116645261+Arjayy007@users.noreply.github.com>
2025-04-10 22:58:11 +0800

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+      <h1>kupal kabah boss</h1>
+      <Button variant="outline">tryy </Button>
+    </>
+  )
+}
+
+export default App
