fbbfa897e5335a7094e54f819cceecace26e7520
Arjayy007 <116645261+Arjayy007@users.noreply.github.com>
2025-04-20 23:14:25 +0800

    Hero Section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,30 @@
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
+import { FaSquareInstagram } from 'react-icons/fa6'
+import { GiSplash } from 'react-icons/gi'
 import meImage from '../assets/me.png'
 import { ScratchToReveal } from "./magicui/scratch-to-reveal"
 import { SpinningText } from './magicui/spinning-text'
 
 const Hero = () => {
   return (
-    <div>
-      <SpinningText radius={19} className='mt-70'>Scratch to Reveal . Scratch to Reveal . Scratch to Reveal .</SpinningText>
-      <ScratchToReveal width={300} height={300} className='rounded-full overflow-hidden -mt-37'>
-       <img src={meImage} alt="saf" />
+    <div className="flex flex-col items-center justify-center mt-60">
+      <SpinningText radius={16}>Scratch to Reveal . Scratch to Reveal . Scratch to Reveal .</SpinningText>
+      <ScratchToReveal width={250} height={250} className='flex flex-col items-center rounded-full overflow-hidden -mt-31'>
+        <img src={meImage} alt="saf" />
       </ScratchToReveal>
-      
+
+      <div className="text-center mt-10 flex flex-col items-center gap-1">
+        <h1 className="text-4xl sm:text-6xl md:text-7xl font-bold text-gray-800 flex items-center gap-3 mt-3 ">Hi, I'm Sustiii <GiSplash /></h1>
+        <h2 className="text-2xl text-gray-600">A Frontend Developer</h2>
+        <p className="text-gray-500 mt-4 max-w-2xl">
+          Computer Science student passionate about frontend development. I love learning new technologies and collaborating to create seamless digital experiences. </p>
+      </div>
+
+      <div className='flex flex-row items-center gap-7 mt-10 text-3xl'>
+      <a aria-label='LinkedIn Profile' target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/sustiguer-rodel-jr-dv-00155b291"> <FaLinkedin /> </a>
+      <a aria-label='Github Acc' target='_blank' rel='noopener noreferrer' href="https://github.com/Arjayy007"><FaGithubSquare /> </a>
+      <a aria-label='Instagram Acc' target='_blank' rel='noopener noreferrer' href="https://www.instagram.com/arjay.777/"><FaSquareInstagram /> </a>
+      </div>
     </div>
   )
 }
