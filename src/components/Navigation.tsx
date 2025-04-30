ff0542c69658f24ac26019f50b5c6abe78feb7c5
Arjayy007 <116645261+Arjayy007@users.noreply.github.com>
2025-04-30 14:17:08 +0800

    fix nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,23 +3,23 @@ import { GoHomeFill } from "react-icons/go"
 const Navigation = () => {
     return (
         <nav className="fixed justify-center w-fit top-0 -m-8 z-20">
-            <div className="flex items-center justify-center bg-gray-100  rounded-full px-8 pt-10 pb-2">
+            <div className="flex items-center justify-center bg-gray-100  rounded-full px-8 pt-10 pb-2 gap-4 md:gap-6">
 
-                <a href="#skills" className="font-bold text-base mr-8 hover:text-gray-400 transition-colors">
+                <a href="#skills" className="font-bold text-sm md:text-base  hover:text-gray-400 transition-colors">
                     Skills
                 </a>
-                <a href="#projects" className="font-bold text-base mr-8 hover:text-gray-400 transition-colors">
+                <a href="#projects" className="font-bold text-sm md:text-base hover:text-gray-400 transition-colors">
                     Projects
                 </a>
 
-                <a href="/" className="mx-2 hover:text-gray-400 transition-colors" title="Home">
-                    <GoHomeFill className="text-4xl" />
+                <a href="/" className=" hover:text-gray-400 transition-colors" title="Home">
+                    <GoHomeFill className="text-2xl md:text-4xl" />
                 </a>
 
-                <a href="#certi" className="font-bold text-base ml-8 hover:text-gray-400 transition-colors">
+                <a href="#certi" className="font-bold text-sm md:text-base  hover:text-gray-400 transition-colors">
                     Certi
                 </a>
-                <a href="#contact" className="font-bold text-base ml-8 hover:text-gray-400 transition-colors">
+                <a href="#contact" className="font-bold text-sm md:text-base  hover:text-gray-400 transition-colors">
                     Contact
                 </a>
 
